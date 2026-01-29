a5fa838aeac5f66096770f3f661154e6c39ed970
Edgar Chen <echen@mozilla.com>
2026-01-29 19:58:10 +0000

    Bug 1994763 - Ignore UAWidget when computing the offset iin nsContentUtils::GetFlattenedTreeAncestorsAndOffsets(); r=masayuki
    The UAWidget is ignored when computing the ancestors, so we should do the same
    when computing the offsets.
    
    Differential Revision: https://phabricator.services.mozilla.com/D277672

--- dom/serializers/nsDocumentEncoder.cpp
+++ dom/serializers/nsDocumentEncoder.cpp
@@ -1234,6 +1234,16 @@ nsDocumentEncoder::RangeSerializer::SerializeNodePartiallyContainedInRange(
   return NS_OK;
 }
 
+static nsINode* GetChildAtInFlatTreeForSelection(const nsINode& aNode,
+                                                 const uint32_t aIndex) {
+  if (ShadowRoot* shadowRoot = aNode.GetShadowRoot()) {
+    if (shadowRoot->IsUAWidget()) {
+      return aNode.GetChildAt_Deprecated(aIndex);
+    }
+  }
+  return aNode.GetChildAtInFlatTree(aIndex);
+}
+
 nsresult nsDocumentEncoder::RangeSerializer::SerializeChildrenOfContent(
     nsIContent& aContent, uint32_t aStartOffset, uint32_t aEndOffset,
     const nsRange* aRange, int32_t aDepth) {
@@ -1252,7 +1262,7 @@ nsresult nsDocumentEncoder::RangeSerializer::SerializeChildrenOfContent(
 
   nsINode* childAsNode =
       mAllowCrossShadowBoundary == AllowRangeCrossShadowBoundary::Yes
-          ? aContent.GetChildAtInFlatTree(aStartOffset)
+          ? GetChildAtInFlatTreeForSelection(aContent, aStartOffset)
           : aContent.GetChildAt_Deprecated(aStartOffset);
 
   MOZ_ASSERT_IF(childAsNode, childAsNode->IsContent());
