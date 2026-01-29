09c0ab9f3a3e3f9eb59fe760adf6592d14d0c417
AndiAJ <andiaj@users.noreply.github.com>
2026-01-29 16:04:26 +0000

    Bug 2013286 - Fix for compose snapshot state being observed from multiple threads when opening the main menu r=aaronmt
    Added composeTestRule.waitForIdle() after clicking the menu to ensure Compose finishes recomposition and drawing before continuing the test and also removed the UIAutomator assertion.
    
    verifyAddonsCanBeUninstalledTest - successfully passed 150x on Firebase ✅
    
    All UI tests successfully passed 2x on Firebase ✅
    
    Differential Revision: https://phabricator.services.mozilla.com/D280946

--- mobile/android/fenix/app/src/androidTest/java/org/mozilla/fenix/ui/robots/HomeScreenRobot.kt
+++ mobile/android/fenix/app/src/androidTest/java/org/mozilla/fenix/ui/robots/HomeScreenRobot.kt
@@ -562,11 +562,9 @@ class HomeScreenRobot(private val composeTestRule: ComposeTestRule) {
             composeTestRule.onNodeWithContentDescription(getStringResource(R.string.content_description_menu)).performClick()
             Log.i(TAG, "openThreeDotMenu: Clicked main menu button")
 
-            composeTestRule.runOnIdle {
-                Log.i(TAG, "runOnIdle: Compose is idle, thread=${Thread.currentThread().name}")
-            }
-
-            assertUIObjectExists(itemWithResId("$packageName:id/design_bottom_sheet"))
+            Log.i(TAG, "openThreeDotMenu: Waiting for compose rule to be idle")
+            composeTestRule.waitForIdle()
+            Log.i(TAG, "openThreeDotMenu: Waited for compose rule to be idle")
 
             ThreeDotMenuMainRobot(composeTestRule).interact()
             return ThreeDotMenuMainRobot.Transition(composeTestRule)
