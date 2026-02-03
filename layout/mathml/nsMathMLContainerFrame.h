96ed4866faf1e232349b91890c8f99ce2aa3f8f5
Frédéric Wang <fwang@igalia.com>
2026-02-03 15:48:29 +0000

    Bug 2013273 - Make GetPreferredStretchSize calculation mode an enum class. r=layout-reviewers,dshin
    STRETCH_CONSIDER_ACTUAL_SIZE is actually not used anymore, so only keep two
    calculation modes.
    
    Differential Revision: https://phabricator.services.mozilla.com/D281186

--- layout/mathml/nsMathMLContainerFrame.h
+++ layout/mathml/nsMathMLContainerFrame.h
@@ -27,11 +27,6 @@ class PresShell;
  * to position children in various customized ways.
  */
 
-// Options for the preferred size at which to stretch our stretchy children
-#define STRETCH_CONSIDER_ACTUAL_SIZE 0x00000001  // just use our current size
-#define STRETCH_CONSIDER_EMBELLISHMENTS \
-  0x00000002  // size calculations include embellishments
-
 class nsMathMLContainerFrame : public nsContainerFrame, public nsMathMLFrame {
  public:
   nsMathMLContainerFrame(ComputedStyle* aStyle, nsPresContext* aPresContext,
@@ -189,7 +184,12 @@ class nsMathMLContainerFrame : public nsContainerFrame, public nsMathMLFrame {
 
   // helper to get the preferred size that a container frame should use to fire
   // the stretch on its stretchy child frames.
-  void GetPreferredStretchSize(DrawTarget* aDrawTarget, uint32_t aOptions,
+  enum class PreferredStretchSizeMode {
+    Embellishments,
+    EmbellishmentsIfSameStretchDirection,
+  };
+  void GetPreferredStretchSize(DrawTarget* aDrawTarget,
+                               PreferredStretchSizeMode aMode,
                                StretchDirection aStretchDirection,
                                nsBoundingMetrics& aPreferredStretchSize);
 
