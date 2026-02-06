6a1e134a7f358db47daaff147ba497124519a16c
Thomas Wisniewski <twisniewski@mozilla.com>
2026-02-06 17:09:21 +0000

    Bug 1950282 - update our webcompat intervention for f1tv.formula1.com; r=webcompat-reviewers,ksenia
    Differential Revision: https://phabricator.services.mozilla.com/D281874

--- testing/webcompat/interventions/tests/test_1950282_f1tv_formula1_com.py
+++ testing/webcompat/interventions/tests/test_1950282_f1tv_formula1_com.py
@@ -2,8 +2,9 @@ import pytest
 
 URL = "https://f1tv.formula1.com/"
 
-SUPPORTED_TEXT = "Watch"
-UNSUPPORTED_TEXT = "Unsupported Browser"
+SUPPORTED_TEXT = "Watch now"
+UNSUPPORTED_TEXT = "browser is not supported"
+USE_APP_TEXT = "Use F1 TV App"
 
 
 @pytest.mark.asyncio
@@ -18,5 +19,12 @@ async def test_enabled(client):
 @pytest.mark.without_interventions
 async def test_disabled(client):
     await client.navigate(URL, wait="none")
-    assert client.await_text(UNSUPPORTED_TEXT, is_displayed=True)
+    use_app, unsupported = client.await_first_element_of(
+        [
+            client.text(USE_APP_TEXT),
+            client.text(UNSUPPORTED_TEXT),
+        ],
+        is_displayed=True,
+    )
+    assert use_app or unsupported
     assert not client.find_text(SUPPORTED_TEXT, is_displayed=True)
