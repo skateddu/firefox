c3a52cf32743444db394d3756b8d5d5efc0e5153
Jeff Boek <jboek@mozilla.com>
2026-02-10 21:19:40 +0000

    Bug 2015938 - Adds gps_integrity_token to _get_secrets_keys_and_target_files r=jonalmeida
    Differential Revision: https://phabricator.services.mozilla.com/D282714

--- taskcluster/android_taskgraph/transforms/build_android_app.py
+++ taskcluster/android_taskgraph/transforms/build_android_app.py
@@ -111,6 +111,7 @@ def _get_secrets_keys_and_target_files(task):
         ("sentry_dsn", ".sentry_token"),
         ("mls", ".mls_token"),
         ("nimbus_url", ".nimbus"),
+        ("gps_integrity_token", ".gps_integrity_token"),
     ]
 
     if task["name"].startswith("fenix-"):
