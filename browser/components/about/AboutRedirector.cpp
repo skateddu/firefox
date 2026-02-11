35c67d91b9a1c619aa3d2bce4b5ef9d145b93647
Shane Hughes <shughes@mozilla.com>
2026-02-11 07:40:25 +0000

    Bug 2015305 r=emcminn,omc-reviewers,Gijs
    Differential Revision: https://phabricator.services.mozilla.com/D282533

--- browser/components/about/AboutRedirector.cpp
+++ browser/components/about/AboutRedirector.cpp
@@ -116,6 +116,7 @@ static const RedirEntry kRedirMap[] = {
     {"messagepreview",
      "chrome://browser/content/messagepreview/messagepreview.html",
      nsIAboutModule::URI_MUST_LOAD_IN_CHILD |
+         nsIAboutModule::URI_CAN_LOAD_IN_PRIVILEGEDABOUT_PROCESS |
          nsIAboutModule::URI_SAFE_FOR_UNTRUSTED_CONTENT |
          nsIAboutModule::ALLOW_SCRIPT | nsIAboutModule::HIDE_FROM_ABOUTABOUT},
     {"settings", "chrome://browser/content/preferences/preferences.xhtml",
--- browser/components/messagepreview/tests/browser/browser_messagepreview_show_spotlight.js
+++ browser/components/messagepreview/tests/browser/browser_messagepreview_show_spotlight.js
@@ -4,6 +4,8 @@ const { AboutMessagePreviewParent } = ChromeUtils.importESModule(
   "resource:///actors/AboutWelcomeParent.sys.mjs"
 );
 
+let messageSandbox;
+
 const TEST_SPOTLIGHT_MESSAGE = {
   id: "TEST_SPOTLIGHT_MESSAGE",
   template: "spotlight",
@@ -50,19 +52,21 @@ const TEST_SPOTLIGHT_MESSAGE = {
   targeting: "true",
 };
 
-add_task(async function test_show_spotlight_message() {
-  const messageSandbox = sinon.createSandbox();
-  let { cleanup, browser } = await openMessagePreviewTab();
-  let aboutMessagePreviewActor = await getAboutMessagePreviewParent(browser);
-  messageSandbox.spy(aboutMessagePreviewActor, "showMessage");
+add_setup(async function () {
+  messageSandbox = sinon.createSandbox();
   registerCleanupFunction(() => {
     messageSandbox.restore();
   });
+});
 
-  await aboutMessagePreviewActor.receiveMessage({
-    name: "MessagePreview:SHOW_MESSAGE",
-    data: JSON.stringify(TEST_SPOTLIGHT_MESSAGE),
-  });
+add_task(async function test_show_spotlight_message() {
+  let { cleanup, browser } = await openMessagePreviewTab();
+  let aboutMessagePreviewActor = await getAboutMessagePreviewParent(browser);
+  messageSandbox.spy(aboutMessagePreviewActor, "showMessage");
+
+  await SpecialPowers.spawn(browser, [TEST_SPOTLIGHT_MESSAGE], message =>
+    content.wrappedJSObject.MPShowMessage(JSON.stringify(message))
+  );
 
   const { callCount } = aboutMessagePreviewActor.showMessage;
   Assert.greaterOrEqual(callCount, 1, "showMessage was called");
@@ -87,5 +91,6 @@ add_task(async function test_show_spotlight_message() {
   await waitForClick("button.primary", win);
   await dialogClosed(browser);
 
+  messageSandbox.restore();
   await cleanup();
 });
