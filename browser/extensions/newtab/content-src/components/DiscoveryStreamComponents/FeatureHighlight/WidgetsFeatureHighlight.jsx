2fd8ba55b1d105cb7288eee806dc894a3e957cb9
Maxx Crawford <mcrawford@mozilla.com>
2026-02-13 16:48:43 +0000

    Bug 2016536 - Add conditional logic to WidgetsFeatureHighlight to set text, image via Nimbus r=home-newtab-reviewers,reemhamz
    Differential Revision: https://phabricator.services.mozilla.com/D283131

--- browser/extensions/newtab/content-src/components/DiscoveryStreamComponents/FeatureHighlight/WidgetsFeatureHighlight.jsx
+++ browser/extensions/newtab/content-src/components/DiscoveryStreamComponents/FeatureHighlight/WidgetsFeatureHighlight.jsx
@@ -6,6 +6,7 @@ import { useSelector } from "react-redux";
 import { FeatureHighlight } from "./FeatureHighlight";
 
 function WidgetsFeatureHighlight({ handleDismiss, handleBlock, dispatch }) {
+  // Extract the strings and feature ID from OMC
   const { messageData } = useSelector(state => state.Messages);
 
   return (
@@ -15,15 +16,38 @@ function WidgetsFeatureHighlight({ handleDismiss, handleBlock, dispatch }) {
       openedOverride={true}
       showButtonIcon={false}
       feature={messageData?.content?.feature}
-      modalClassName="widget-highlight-wrapper"
+      modalClassName={`widget-highlight-wrapper${messageData.content?.hideImage ? " no-image" : ""}`}
       message={
         <div className="widget-highlight">
-          <img
-            src="chrome://newtab/content/data/content/assets/widget-message.png"
-            alt=""
-          />
-          <h3 data-l10n-id="newtab-widget-message-title" />
-          <p data-l10n-id="newtab-widget-message-copy" />
+          {!messageData.content?.hideImage && (
+            <img
+              src={
+                messageData.content?.imageURL ||
+                "chrome://newtab/content/data/content/assets/widget-message.png"
+              }
+              alt=""
+            />
+          )}
+          {messageData.content?.cardTitle ? (
+            <h3 className="title">{messageData.content.cardTitle}</h3>
+          ) : (
+            <h3
+              className="title"
+              data-l10n-id={
+                messageData.content.title || "newtab-widget-message-title"
+              }
+            />
+          )}
+          {messageData.content?.cardMessage ? (
+            <p className="subtitle">{messageData.content.cardMessage}</p>
+          ) : (
+            <p
+              className="subtitle"
+              data-l10n-id={
+                messageData.content.subtitle || "newtab-widget-message-copy"
+              }
+            />
+          )}
         </div>
       }
       dispatch={dispatch}
--- browser/extensions/newtab/data/content/activity-stream.bundle.js
+++ browser/extensions/newtab/data/content/activity-stream.bundle.js
@@ -13277,6 +13277,7 @@ function WidgetsFeatureHighlight({
   handleBlock,
   dispatch
 }) {
+  // Extract the strings and feature ID from OMC
   const {
     messageData
   } = (0,external_ReactRedux_namespaceObject.useSelector)(state => state.Messages);
@@ -13286,16 +13287,22 @@ function WidgetsFeatureHighlight({
     openedOverride: true,
     showButtonIcon: false,
     feature: messageData?.content?.feature,
-    modalClassName: "widget-highlight-wrapper",
+    modalClassName: `widget-highlight-wrapper${messageData.content?.hideImage ? " no-image" : ""}`,
     message: /*#__PURE__*/React.createElement("div", {
       className: "widget-highlight"
-    }, /*#__PURE__*/React.createElement("img", {
-      src: "chrome://newtab/content/data/content/assets/widget-message.png",
+    }, !messageData.content?.hideImage && /*#__PURE__*/React.createElement("img", {
+      src: messageData.content?.imageURL || "chrome://newtab/content/data/content/assets/widget-message.png",
       alt: ""
-    }), /*#__PURE__*/React.createElement("h3", {
-      "data-l10n-id": "newtab-widget-message-title"
-    }), /*#__PURE__*/React.createElement("p", {
-      "data-l10n-id": "newtab-widget-message-copy"
+    }), messageData.content?.cardTitle ? /*#__PURE__*/React.createElement("h3", {
+      className: "title"
+    }, messageData.content.cardTitle) : /*#__PURE__*/React.createElement("h3", {
+      className: "title",
+      "data-l10n-id": messageData.content.title || "newtab-widget-message-title"
+    }), messageData.content?.cardMessage ? /*#__PURE__*/React.createElement("p", {
+      className: "subtitle"
+    }, messageData.content.cardMessage) : /*#__PURE__*/React.createElement("p", {
+      className: "subtitle",
+      "data-l10n-id": messageData.content.subtitle || "newtab-widget-message-copy"
     })),
     dispatch: dispatch,
     dismissCallback: () => {
