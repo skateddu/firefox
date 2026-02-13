b05aa16b1006ca2bde0456ebf7d3e687b3d4f616
Matthew Gaudet <mgaudet@mozilla.com>
2026-02-13 16:45:48 +0000

    Bug 2016513 - Remove invalid debugger assertion around observability in light of multiple debuggers. r=arai
    As the test case shows, we can get into a situation where a script is still a
    debuggee, but we've got a mismatch on the observing flag because we've got more
    than one debugger involved.
    
    Differential Revision: https://phabricator.services.mozilla.com/D283092

--- js/src/debugger/Debugger.cpp
+++ js/src/debugger/Debugger.cpp
@@ -3360,7 +3360,6 @@ static bool UpdateExecutionObservabilityOfScriptsInZone(
   // BaselineScripts. This must be done as a separate phase as we can only
   // discard the BaselineScript on scripts that have no IonScript.
   for (size_t i = 0; i < scripts.length(); i++) {
-    MOZ_ASSERT_IF(scripts[i]->isDebuggee(), observing);
     if (!scripts[i]->jitScript()->icScript()->active()) {
       FinishDiscardBaselineScript(gcx, scripts[i]);
     }
