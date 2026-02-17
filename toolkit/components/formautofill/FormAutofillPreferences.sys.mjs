782bad0ace19f6b7e0fe044aad638ac359549273
Tanvi Manku <tanvi.manku@gmail.com>
2026-02-17 19:54:35 +0000

    Bug 2001254: Add basic tests for "Manage addresses" subpage. r=mtigley,credential-management-reviewers
    Differential Revision: https://phabricator.services.mozilla.com/D276937

--- toolkit/components/formautofill/FormAutofillPreferences.sys.mjs
+++ toolkit/components/formautofill/FormAutofillPreferences.sys.mjs
@@ -346,6 +346,7 @@ export class FormAutofillPreferences {
           },
           options: [
             {
+              id: "delete-address-button",
               control: "moz-button",
               iconSrc: "chrome://global/skin/icons/delete.svg",
               type: "icon",
@@ -357,6 +358,7 @@ export class FormAutofillPreferences {
               },
             },
             {
+              id: "edit-address-button",
               control: "moz-button",
               iconSrc: "chrome://global/skin/icons/edit.svg",
               type: "icon",
