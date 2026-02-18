3d2cc9338a6038b41b6bcc79dc413fca0af49a0c
Mark Banner <standard8@mozilla.com>
2026-02-18 17:49:54 +0000

    Bug 2017131 - Include SearchUITestUtils.sys.mjs in TypeScript checking for browser/components/search. r=search-reviewers,scunnane
    Differential Revision: https://phabricator.services.mozilla.com/D283598

--- browser/components/search/test/SearchUITestUtils.sys.mjs
+++ browser/components/search/test/SearchUITestUtils.sys.mjs
@@ -2,9 +2,9 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-const lazy = {};
+import { XPCOMUtils } from "resource://gre/modules/XPCOMUtils.sys.mjs";
 
-ChromeUtils.defineESModuleGetters(lazy, {
+const lazy = XPCOMUtils.declareLazy({
   BrowserSearchTelemetry:
     "moz-src:///browser/components/search/BrowserSearchTelemetry.sys.mjs",
   TelemetryTestUtils: "resource://testing-common/TelemetryTestUtils.sys.mjs",
