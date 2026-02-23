108cd78fd344a901cd861c80ce4b4149114bb4cc
mark <1446916+markcellus@users.noreply.github.com>
2026-02-23 20:37:27 +0000

    Bug 2018560 - Prevent incorrect value after cancelling 'Use other' application option r=hjones
    Differential Revision: https://phabricator.services.mozilla.com/D284470

--- browser/components/preferences/main.js
+++ browser/components/preferences/main.js
@@ -7907,16 +7907,16 @@ const AppFileHandler = (function () {
 
             let originalValue = handlerItem.actionsMenu.value;
 
-            handlerItem.actionsMenu.addEventListener("change", async e => {
+            handlerItem.actionsMenu.addEventListener("change", async () => {
               const newValue = handlerItem.actionsMenu.value;
 
-              if (newValue !== "choose-app" && newValue !== "manage-app") {
-                /**
-                 * Must explicitly wait for MozSelect to update the value
-                 * here, because sometimes it hasn't updated yet.
-                 */
-                await handlerItem.actionsMenu.updateComplete;
+              /**
+               * Must explicitly wait for MozSelect to update the value
+               * here, because sometimes it hasn't updated yet.
+               */
+              await handlerItem.actionsMenu.updateComplete;
 
+              if (newValue !== "choose-app" && newValue !== "manage-app") {
                 this._onSelectActionsMenuOption(handlerItem);
               } else {
                 /**
@@ -7925,19 +7925,12 @@ const AppFileHandler = (function () {
                  */
                 handlerItem.actionsMenu.value = originalValue;
 
-                /**
-                 * Prevent change notification to any parent elements.
-                 */
-                e.stopPropagation();
-
                 if (newValue === "choose-app") {
                   this.chooseApp(handlerItem);
                 } else {
                   this.manageApp(handlerItem);
                 }
               }
-
-              originalValue = newValue;
             });
           })
         );
