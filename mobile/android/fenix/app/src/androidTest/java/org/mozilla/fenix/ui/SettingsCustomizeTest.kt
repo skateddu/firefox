d91614039a7ed420dcc45cfda068726215c1a9c2
Delia Pop <dpop@mozilla.com>
2026-02-23 14:42:19 +0000

    Bug 2018626 - Disable the verifyTheToolbarLayoutSectionTest UI test r=ajoltan
    Disabling the verifyTheToolbarLayoutSectionTest UI test for causing failures and fixing it offline.
    
    Differential Revision: https://phabricator.services.mozilla.com/D284455

--- mobile/android/fenix/app/src/androidTest/java/org/mozilla/fenix/ui/SettingsCustomizeTest.kt
+++ mobile/android/fenix/app/src/androidTest/java/org/mozilla/fenix/ui/SettingsCustomizeTest.kt
@@ -6,6 +6,7 @@ package org.mozilla.fenix.ui
 
 import android.content.res.Configuration
 import androidx.compose.ui.test.junit4.AndroidComposeTestRule
+import org.junit.Ignore
 import org.junit.Rule
 import org.junit.Test
 import org.mozilla.fenix.customannotations.SkipLeaks
@@ -177,6 +178,7 @@ class SettingsCustomizeTest : TestSetup() {
     }
 
     // TestRail link: https://mozilla.testrail.io/index.php?/cases/view/3333174
+    @Ignore("Failing, see: https://bugzilla.mozilla.org/show_bug.cgi?id=2018626")
     @Test
     fun verifyTheToolbarLayoutSectionTest() {
         homeScreen(composeTestRule) {
