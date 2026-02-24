365b3458c8de00d4ca1fc4ca9aad13aa8836301c
Alex Hochheiden <ahochheiden@mozilla.com>
2026-02-24 20:01:05 +0000

    Bug 2018327 - Run `_collect_source_mtimes` in a background thread during `FileCopier.copy()` r=firefox-build-system-reviewers,sergesanspaille
    Yields ~60ms reduced runtime on Windows. No discernable impact on macOS.
    
    Differential Revision: https://phabricator.services.mozilla.com/D284605

--- python/mozbuild/mozpack/copier.py
+++ python/mozbuild/mozpack/copier.py
@@ -370,6 +370,20 @@ class FileCopier(FileRegistry):
         have_symlinks = hasattr(os, "symlink")
         destination = os.path.normpath(destination)
 
+        dest_mtimes = {}
+        dest_symlinks = {}
+        src_mtimes = {}
+        src_dirs = set()
+
+        # _collect_source_mtimes scans source directories and is independent
+        # of the destination setup and walk, so start it in a background
+        # thread immediately.  Filesystem syscalls release the GIL, so it
+        # runs concurrently with the makedirs and destination scan below.
+        src_future = None
+        if skip_if_older and len(self) > 100:
+            src_executor = futures.ThreadPoolExecutor(1)
+            src_future = src_executor.submit(_collect_source_mtimes, self)
+
         # We create the destination directory specially. We can't do this as
         # part of the loop doing mkdir() below because that loop munges
         # symlinks and permissions and parent directories of the destination
@@ -422,11 +436,6 @@ class FileCopier(FileRegistry):
                 os.umask(umask)
                 os.chmod(d, 0o777 & ~umask)
 
-        dest_mtimes = {}
-        dest_symlinks = {}
-        src_mtimes = {}
-        src_dirs = set()
-
         if isinstance(remove_unaccounted, FileRegistry):
             existing_files = set(
                 os.path.normpath(os.path.join(destination, p))
@@ -457,8 +466,9 @@ class FileCopier(FileRegistry):
                         existing_files.discard(path)
                         del dest_symlinks[path]
 
-        if skip_if_older and len(self) > 100:
-            src_mtimes, src_dirs = _collect_source_mtimes(self)
+        if src_future is not None:
+            src_mtimes, src_dirs = src_future.result()
+            src_executor.shutdown(wait=False)
 
         # Now we reconcile the state of the world against what we want.
         dest_files = set()
