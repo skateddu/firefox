12a936cfa754f5f976c931b39514bf3bb17599e3
Irene Ni <ini@mozilla.com>
2026-02-24 16:16:37 +0000

    Bug 2018788 - Turn UK Sections on by default. r=home-newtab-reviewers,nbarrett
    Differential Revision: https://phabricator.services.mozilla.com/D284552

--- browser/app/profile/firefox.js
+++ browser/app/profile/firefox.js
@@ -2046,9 +2046,9 @@ pref("browser.newtabpage.activity-stream.discoverystream.topicSelection.locale-t
 pref("browser.newtabpage.activity-stream.discoverystream.topicLabels.locale-topic-label-config", "en-US, en-GB, en-CA");
 
 // List of locales that get section layout by default
-pref("browser.newtabpage.activity-stream.discoverystream.sections.locale-content-config", "en-US,en-CA");
+pref("browser.newtabpage.activity-stream.discoverystream.sections.locale-content-config", "en-US,en-CA,en-GB");
 // List of regions that get section layout by default
-pref("browser.newtabpage.activity-stream.discoverystream.sections.region-content-config", "US");
+pref("browser.newtabpage.activity-stream.discoverystream.sections.region-content-config", "US,GB");
 
 pref("browser.newtabpage.activity-stream.discoverystream.sections.cards.enabled", true);
 
