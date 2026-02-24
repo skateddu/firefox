08e6e586ce959ce277498bcd36a260f06ea43304
Ryan VanderMeulen <rvandermeulen@mozilla.com>
2026-02-24 23:11:07 +0000

    Bug 2018952 - Switch linux64-upx toolchain to fetched source. r=firefox-build-system-reviewers,ahochheiden
    Differential Revision: https://phabricator.services.mozilla.com/D284661

--- taskcluster/scripts/misc/build-upx.sh
+++ taskcluster/scripts/misc/build-upx.sh
@@ -1,26 +1,15 @@
 #!/bin/bash
 set -x -e -v
 
-WORKSPACE=$HOME/workspace
-INSTALL_DIR=$WORKSPACE/upx
+PROJECT="upx"
 
-mkdir -p $INSTALL_DIR/bin
+pushd "${MOZ_FETCHES_DIR}/${PROJECT}"
+make -j$(nproc)
+popd
 
-cd $WORKSPACE
+mkdir -p "${PROJECT}/bin"
+mv "${MOZ_FETCHES_DIR}/${PROJECT}/build/release/upx" "${PROJECT}/bin/"
+tar -acf "${PROJECT}.tar.zst" "${PROJECT}"
 
-git clone -n https://github.com/upx/upx.git upx-clone
-cd upx-clone
-# https://github.com/upx/upx/releases/tag/v3.95
-git checkout 7a3637ff5a800b8bcbad20ae7f668d8c8449b014 # Asserts integrity of the clone (right?)
-git submodule update --init --recursive
-cd src
-make -j$(nproc) CXXFLAGS_WERROR=
-cp upx.out $INSTALL_DIR/bin/upx
-
-# --------------
-
-cd $WORKSPACE
-tar caf upx.tar.zst upx
-
-mkdir -p $UPLOAD_DIR
-cp upx.tar.* $UPLOAD_DIR
+mkdir -p "$UPLOAD_DIR"
+mv "${PROJECT}.tar.zst" "$UPLOAD_DIR"
