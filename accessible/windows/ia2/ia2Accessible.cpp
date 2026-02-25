c49e3a71434682134fd674dd0d9a6c7750a25527
James Teh <jteh@mozilla.com>
2026-02-25 01:56:50 +0000

    Bug 1938631: Expose text-model:a1 IAccessible2 object attribute on editable text controls so that JAWS works correctly with role=combobox on the address bar. r=morgan
    Differential Revision: https://phabricator.services.mozilla.com/D281722

--- accessible/windows/ia2/ia2Accessible.cpp
+++ accessible/windows/ia2/ia2Accessible.cpp
@@ -451,6 +451,17 @@ ia2Accessible::get_attributes(BSTR* aAttributes) {
     // IAccessible2 expects heading level to be exposed as an object attribute.
     // However, all other group position info is exposed via groupPosition.
     nsAccUtils::SetAccGroupAttrs(attributes, acc);
+  } else if (acc->IsEditableRoot()) {
+    // JAWS requires text-model:a1 to be specified on editable text controls
+    // with a role other than ROLE_SYSTEM_TEXT outside of web content in order
+    // for it to use IAccessibleText. For example, this is needed for the
+    // Firefox address bar, which has role="combobox". Although we don't really
+    // need to expose it on editable text controls beyond this specific case,
+    // there's no harm in doing so and Chromium also does this. This attribute
+    // is documented here:
+    // https://wiki.linuxfoundation.org/accessibility/ia2/ia2_implementation_guide#iaccessibletext_model
+    RefPtr<nsAtom> textModel = NS_Atomize("text-model");
+    attributes->SetAttributeStringCopy(textModel, u"a1"_ns);
   }
   return ConvertToIA2Attributes(attributes, aAttributes);
 }
