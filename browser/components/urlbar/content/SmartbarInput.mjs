79e76ee0440c981c970bf549b1c083eb042cb38a
Morgan Rae Reschenberg <mreschenberg@berkeley.edu>
2026-02-25 01:56:50 +0000

    Bug 1938631: Move role=combobox from moz-input box to urlbar-input for URL bar r=Jamie,urlbar-reviewers,Standard8
    This makes the URL bar accessible to Voice Control, and it doesn't break VoiceOver, although it does change what VO speaks from "Search google or enter address, edit text" to "search google or enter address, combobox".
    
    The behaviour this permits for Voice Control users via number navigation is still distinct from the behaviour they'll get when navigating by grid.
    
    With this change, VC users that say "Click <URL bar number>" will see the URL bar get focus. The text within the URL bar will not be selected, and the text indicator cursor will blink at the end of the URL input, after all existing text. Users can interact with the text and the URL bar with traditional VC commands ("select all", "delete that", "google dot com", "enter that"). They can also show the expanded combobox with "Move down".
    
    When navigating by grid, VC users will see the URL bar get focus, all the text in the URL bar will be selected, and the URL bar drop down will expand. The same text-edit and navigation commands above work.
    
    Differential Revision: https://phabricator.services.mozilla.com/D280866

--- browser/components/urlbar/content/SmartbarInput.mjs
+++ browser/components/urlbar/content/SmartbarInput.mjs
@@ -157,14 +157,13 @@ export class SmartbarInput extends HTMLElement {
         <moz-urlbar-slot name="site-info"> </moz-urlbar-slot>
         <moz-input-box tooltip="aHTMLTooltip"
                        class="urlbar-input-box"
-                       flex="1"
-                       role="combobox"
-                       aria-owns="urlbar-results">
+                       flex="1">
           <html:input id="urlbar-scheme"
                       required="required"/>
           <html:input id="urlbar-input"
                       class="urlbar-input textbox-input"
                       aria-controls="urlbar-results"
+                      role="combobox"
                       aria-autocomplete="both"
                       inputmode="mozAwesomebar"
                       data-l10n-id="smartbar-placeholder"/>
