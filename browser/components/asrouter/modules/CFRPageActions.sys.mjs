85ff1210c503cf1fa6a40ceda9b5c9ee878c0089
Atila Butkovits <abutkovits@mozilla.com>
2026-02-27 02:30:00 +0200

    Revert "Bug 2019414 - Add a Nimbus feature for privacy and security messaging. r=omc-reviewers,emcminn" for causing failures complaining about recordExposureEvent.
    This reverts commit b25680a5798c25e865bc8c42c8d2896ceacdf4e3.

--- browser/components/asrouter/modules/CFRPageActions.sys.mjs
+++ browser/components/asrouter/modules/CFRPageActions.sys.mjs
@@ -18,7 +18,6 @@ const lazy = {};
 ChromeUtils.defineESModuleGetters(lazy, {
   CustomizableUI:
     "moz-src:///browser/components/customizableui/CustomizableUI.sys.mjs",
-  NimbusFeatures: "resource://nimbus/ExperimentAPI.sys.mjs",
   PrivateBrowsingUtils: "resource://gre/modules/PrivateBrowsingUtils.sys.mjs",
   RemoteL10n: "resource:///modules/asrouter/RemoteL10n.sys.mjs",
 });
@@ -875,7 +874,6 @@ export class PageAction {
       this.window.document.getElementById(content.anchor_id) || this.container;
 
     await this._renderMilestonePopup(message, browser);
-    lazy.NimbusFeatures.privacySecurityMessaging.recordExposureEvent();
     return true;
   }
 }
