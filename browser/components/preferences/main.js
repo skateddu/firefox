8a289d6c69eb1c44bc2512d53056238405b454a0
hannajones <hjones@mozilla.com>
2026-02-27 04:42:06 +0000

    Bug 1995062 - Part 1: render backup-settings via config r=fluent-reviewers,desktop-theme-reviewers,cdupuis,bolsson,akulyk,tgiles
    Differential Revision: https://phabricator.services.mozilla.com/D279015

--- browser/components/preferences/main.js
+++ browser/components/preferences/main.js
@@ -4821,6 +4821,17 @@ SettingGroupManager.registerGroups({
       },
     ],
   },
+  backup: {
+    l10nId: "settings-data-backup-header2",
+    headingLevel: 2,
+    supportPage: "firefox-backup",
+    items: [
+      {
+        id: "backupSettings",
+        control: "backup-settings",
+      },
+    ],
+  },
   translationsAutomaticTranslation: {
     inProgress: true,
     headingLevel: 2,
